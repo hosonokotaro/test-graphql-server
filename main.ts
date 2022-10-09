7cbafd081088991e5f8d969730cc404d6b21239b
hosono kotaro <hosono1985@gmail.com>
2022-10-09 14:00:38 +0900

    feat: Create apollo server

---
+++ main.ts
@@ -0,0 +1,21 @@
+import { ApolloServer, gql } from "apollo-server";
+
+// NOTE GraphQL Schema の定義
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+// NOTE: Resolver の定義
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+// NOTE: Init GraphQL server
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
