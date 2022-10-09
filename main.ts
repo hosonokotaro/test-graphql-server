2b47c6f61dbc63279d634e5e3c5a363232d4cc19
hosono kotaro <hosono1985@gmail.com>
2022-10-09 14:35:41 +0900

    fix: Apply prettier setting

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from 'apollo-server'
 
 // NOTE GraphQL Schema の定義
 const typeDefs = gql`
   type Query {
     hello: String!
   }
-`;
+`
 
 // NOTE: Resolver の定義
 const resolvers = {
   Query: {
-    hello: () => "world",
+    hello: () => 'world',
   },
-};
+}
 
 // NOTE: Init GraphQL server
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+  console.log(`Server ready at ${url}`)
+})
