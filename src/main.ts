617ab8d71daff89002f8308f473b8635af50997a
hosono kotaro <hosono1985@gmail.com>
2022-11-03 17:33:44 +0900

    Connect API (#1)
    * feat: Add read json file
    
    * feat: Connect Realtime Database
    
    * fix: Remove unused files
    
    * feat: Apply typescript to development environment
    
    * fix: Split schema file
    
    * feat: Add codegen
    
    * feat: Add env setting
    
    * feat: Add eslint
    
    * feat: Add sort plugin
    
    * fix: code format
    
    * fix: Add error validation

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,51 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { addResolversToSchema } from '@graphql-tools/schema'
+import { ApolloServer } from 'apollo-server'
+import axios from 'axios'
+import * as dotenv from 'dotenv'
+import { join } from 'path'
 
-// NOTE GraphQL Schema の定義
-const typeDefs = gql`
-  type Query {
-    hello: String!
-  }
-`
+import { Resolvers } from './types/generated/graphql'
+
+// NOTE: Context type をここで使う場合
+// import { Context } from './types/context'
+
+dotenv.config()
+
+// NOTE: https://www.the-guild.dev/graphql/tools/docs/schema-loading
+const schema = loadSchemaSync(join(__dirname, './schema.graphql'), {
+  loaders: [new GraphQLFileLoader()],
+})
+
+const API_URL = process.env.API_URL
 
 // NOTE: Resolver の定義
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
-    hello: () => 'world',
+    async articles() {
+      try {
+        const { data } = await axios.get(`${API_URL}/article-list.json`)
+        return data
+      } catch (error) {
+        if (error instanceof Error) {
+          return {
+            status: 'error',
+            message: error.message,
+          }
+        }
+
+        console.log(error)
+      }
+    },
   },
 }
 
+const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
+
 // NOTE: Init GraphQL server
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ schema: schemaWithResolvers })
+
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
