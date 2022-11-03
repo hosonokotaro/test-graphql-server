d86d4a2a8a346fda149a0f73c22556aa80399d65
hosono kotaro <hosono1985@gmail.com>
2022-11-03 13:16:55 +0900

    feat: Add env setting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ApolloServer, gql } from 'apollo-server'
 import axios from 'axios'
 import { join } from 'path'
+import * as dotenv from 'dotenv'
 import { loadSchemaSync } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { addResolversToSchema } from '@graphql-tools/schema'
@@ -10,6 +11,8 @@ import { Resolvers } from './types/generated/graphql'
 // NOTE: Context type をここで使う場合
 // import { Context } from './types/context'
 
+dotenv.config()
+
 // NOTE: https://www.the-guild.dev/graphql/tools/docs/schema-loading
 const schema = loadSchemaSync(join(__dirname, './schema.graphql'), {
   loaders: [new GraphQLFileLoader()],
@@ -17,7 +20,7 @@ const schema = loadSchemaSync(join(__dirname, './schema.graphql'), {
 
 // TODO: error type を定義する。return null ではないようにしてみたい
 
-const BASE_URL = 'https://hosonokotaro-blog-98d9d.firebaseio.com'
+const API_URL = process.env.API_URL
 
 // NOTE: Resolver の定義
 const resolvers: Resolvers = {
@@ -25,7 +28,7 @@ const resolvers: Resolvers = {
     // TODO: 型定義を作成して持ってくる
     async articles() {
       try {
-        const { data } = await axios.get(`${BASE_URL}/article-list.json`)
+        const { data } = await axios.get(`${API_URL}/article-list.json`)
         return data
       } catch (error) {
         return null
